011da1998e6646ff7fe3a0356670448019a05965
a7medbehiry <a7medbehiry@gmail.com>
2024-07-23 19:31:56 +0300

    add firebase & make auth repo & make user entity & make errors

---
+++ lib/features/auth/domain/entities/user_entity.dart
@@ -0,0 +1,11 @@
+class UserEntity {
+  final String email;
+  final String password;
+  final String uId;
+
+  UserEntity({
+    required this.email,
+    required this.password,
+    required this.uId,
+  });
+}
