63f6fa3cf419e930ca2e2b9d8d72dd0c8392eef5
a7medbehiry <a7medbehiry@gmail.com>
2024-08-01 23:33:40 +0300

    refactor code

--- lib/features/auth/presentation/views/widgets/sign_in_view_body.dart
+++ lib/features/auth/presentation/views/widgets/sign_in_view_body.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub/constants.dart';
@@ -107,14 +109,20 @@ class _SignInViewBodyState extends State<SignInViewBody> {
               const SizedBox(
                 height: 16,
               ),
-              // SocialLoginButton(
-              //   onPressed: () {},
-              //   image: Assets.imagesApple,
-              //   title: 'تسجيل بواسطة أبل',
-              // ),
-              // const SizedBox(
-              //   height: 16,
-              // ),
+              Platform.isIOS
+                  ? Column(
+                      children: [
+                        SocialLoginButton(
+                          onPressed: () {},
+                          image: Assets.imagesApple,
+                          title: 'تسجيل بواسطة أبل',
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                      ],
+                    )
+                  : const SizedBox(),
               SocialLoginButton(
                 onPressed: () {
                   context.read<SignInCubit>().signInWithFacebook();
